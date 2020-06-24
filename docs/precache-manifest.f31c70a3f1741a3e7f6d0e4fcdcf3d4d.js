ac13555c0398f4883399d575b67ef23e12efb7dc
Michael <mcljs15@gmail.com>
2020-06-24 09:56:50 -0400

    docs agregado

---
+++ docs/precache-manifest.f31c70a3f1741a3e7f6d0e4fcdcf3d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8455b5dd944e2657cb15ce932ccae0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e36a6ccd617b29e5e12",
+    "url": "/static/css/main.4c3c57fb.chunk.css"
+  },
+  {
+    "revision": "5ebe41a1e8551bcde621",
+    "url": "/static/js/2.a930a444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a930a444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e36a6ccd617b29e5e12",
+    "url": "/static/js/main.1cd3acd3.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
